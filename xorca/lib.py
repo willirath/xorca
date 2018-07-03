d944082fd781f5e15ac8f76cffae8efbf532d70a
Jan Klaus Rieck <jrieck@geomar.de>
2018-07-03 12:08:35 +0200

    lib.trim_and_squeeze: the only dimension of size 1 to exist after trim_and_squeeze must be named t and be of dtype datetime64

--- xorca/lib.py
+++ xorca/lib.py
@@ -60,7 +60,9 @@ def trim_and_squeeze(ds,
         ds = ds.isel(x=slice(*x_slice))
     
     to_squeeze = [dim for dim in ds.dims 
-                  if ((ds[dim].size == 1) and (dim is not "t"))]
+                  if ((ds[dim].size == 1) and ((dim is not "t")
+                      or (np.invert(np.issubdtype(ds[dim].dtype, 
+                                                  np.datetime64)))))]
         
     ds = ds.squeeze(dim = to_squeeze)
     return ds
